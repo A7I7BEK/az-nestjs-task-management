ba36db85a6bb1f1b837b26fb7a46f03cb37c89bf
A7I7BEK <A7I7BEK@users.noreply.github.com>
2024-04-25 04:18:26 +0000

    deploy: 82ff33fea5fac23773d0ae459c4c7660cfb3adf6

---
+++ precache-manifest.12c98ee04727078b8687258f67ce71b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259398075ba9d62922d025e1e5083f24",
+    "url": "https://A7I7BEK.github.io/az-nestjs-task-management/index.html"
+  },
+  {
+    "revision": "b907fa9e275aebc5af88",
+    "url": "https://A7I7BEK.github.io/az-nestjs-task-management/static/css/main.0b051ba3.chunk.css"
+  },
+  {
+    "revision": "9a549b15d3e039ef7c79",
+    "url": "https://A7I7BEK.github.io/az-nestjs-task-management/static/js/2.d087f43c.chunk.js"
+  },
+  {
+    "revision": "b907fa9e275aebc5af88",
+    "url": "https://A7I7BEK.github.io/az-nestjs-task-management/static/js/main.dff76382.chunk.js"
+  },
+  {
+    "revision": "886fa88398bbe6e51577",
+    "url": "https://A7I7BEK.github.io/az-nestjs-task-management/static/js/runtime~main.a12cb544.js"
+  }
+]);
